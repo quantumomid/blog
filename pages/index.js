205164bfcb28006b6d8661a930b6f3e6708b38ab
quantumomid <omid.wakili@outlook.com>
2022-02-03 07:34:51 +0000

    add hero section and some dummy styles

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Hero from '../components/Hero'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -12,8 +13,9 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <Hero />
         <h1 className={styles.title}>
-          Welcome to Omid's  blog!
+          {/* Welcome to Omid's  blog! */}
         </h1>
       </main>
 
