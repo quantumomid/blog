9a9fb352fc6a177aa33e28f017506c4aea1454c1
quantumomid <omid.wakili@outlook.com>
2022-02-08 14:52:21 +0000

    use post data from file system in post content

--- components/posts/post-detail/PostContent.js
+++ components/posts/post-detail/PostContent.js
@@ -2,23 +2,23 @@ import PostHeader from "./PostHeader";
 import styles from "./PostContent.module.css";
 import ReactMarkdown from "react-markdown";
 
-const DUMMY_POST = {
-    title: "Getting Started with Next.js",
-    image: "getting-started-with-next-js.png", 
-    excerpt: "Next.js is a React framework for production, which makes building full stack applications easy!", 
-    date: "2021-10-16", 
-    slug: "getting-started-with-next-js",
-    content: "# This is a post"
-}
+// const DUMMY_POST = {
+//     title: "Getting Started with Next.js",
+//     image: "getting-started-with-next-js.png", 
+//     excerpt: "Next.js is a React framework for production, which makes building full stack applications easy!", 
+//     date: "2021-10-16", 
+//     slug: "getting-started-with-next-js",
+//     content: "# This is a post"
+// }
 
 
-const PostContent = () => {
-    const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+const PostContent = ({ post }) => {
+    const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
     return (
         <article className={styles.content}>
-            <PostHeader title={DUMMY_POST.title} image={imagePath} />
-            <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+            <PostHeader title={post.title} image={imagePath} />
+            <ReactMarkdown>{post.content}</ReactMarkdown>
         </article>
     )
 }
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,8 +1,38 @@
 import PostContent from "../../components/posts/post-detail/PostContent";
+import { getPostData, getPostsFiles } from "../../helpers/postsUtils";
 
-const PostDetailPage = () => {
+export const getStaticPaths = () => {
+    const postFileNames = getPostsFiles();
+
+    const slugs = postFileNames.map(filename => filename.replace(/\.md$/, ""));
+    const paths = slugs.map(slug => ({ params: { slug: slug } }))
+    return {
+        paths: paths,
+        fallback: false
+    }
+    // If you DONT want to pregenerate any posts pages i.e. if you have thousands of articles etc.
+    // then do something like below 
+    // return {
+    //     paths: [],
+    //     fallback: true // or "blocking"
+    // }
+}
+
+export const getStaticProps = (context) => {
+    const { params: { slug } } = context;
+    const postData = getPostData(slug);
+    return {
+        props: {
+            post: postData
+        }, 
+        revalidate: 600
+    }
+}
+
+const PostDetailPage = ({ post }) => {
+    // console.log(post);
     return (
-        <PostContent />
+        <PostContent post={post} />
     )
 }
 
