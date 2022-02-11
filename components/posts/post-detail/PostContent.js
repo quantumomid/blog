781c0913bb09744c5c1d0c28d315986a3b5a461e
quantumomid <omid.wakili@outlook.com>
2022-02-11 12:06:19 +0000

    optimise react syntax highlighter imports to reduce bundle sizes! for build and deployment

--- components/posts/post-detail/PostContent.js
+++ components/posts/post-detail/PostContent.js
@@ -2,9 +2,14 @@ import PostHeader from "./PostHeader";
 import styles from "./PostContent.module.css";
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import atomDark from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
 import { Fragment } from "react";
+import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
+import css from "react-syntax-highlighter/dist/cjs/languages/prism/css";
+
+SyntaxHighlighter.registerLanguage("js", js);
+SyntaxHighlighter.registerLanguage("css", css);
 
 const PostContent = ({ post }) => {
     const imagePath = `/images/posts/${post.slug}/${post.image}`;
@@ -43,7 +48,7 @@ const PostContent = ({ post }) => {
             // console.log(code)
             const { className, children } = code;
             const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
-            return <SyntaxHighlighter style={atomDark} language={language} children={children} />
+            return <SyntaxHighlighter style={atomDark} language={language} /*children={children}*/>{children}</SyntaxHighlighter>
         }
     }
 
